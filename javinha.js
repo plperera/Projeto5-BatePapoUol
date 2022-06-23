8b18cb3a76fd45a66981222afc5d993500d4f67c
Pedro Pereira <plp.leoncio@gmail.com>
2022-06-23 17:39:20 -0300

    input quase concluido

--- javinha.js
+++ javinha.js
@@ -2,22 +2,58 @@
 function importarChat(mensagem){
 
     console.log("estou importando o chat");
+    console.log(mensagem.data)
+
     const linhaMensagem = document.querySelector(".caixa-batepapo")
-   
 
     for (let i = 0; i < mensagem.data.length; i++){
-  
-    linhaMensagem.innerHTML += 
-    `
-    <ul class="mensagem ${mensagem.data[i].type}">
 
-    <horario>${mensagem.data[i].time}</horario>
-    <usuario>${mensagem.data[i].from}</usuario>
-    <span>para</span>
-    <usuario>${mensagem.data[i].to}:</usuario>
-    <mensagem>${mensagem.data[i].text}</mensagem>
+        if (mensagem.data[i].text === "sai da sala..." || mensagem.data[i].text === "entra na sala..." ||  mensagem.data[i].text === "entrou na sala..." ){
 
-    </ul>`   
+            ///mensagem de status, usar horario + usuario + textoStatus
+
+                linhaMensagem.innerHTML += 
+                `
+                <ul class="mensagem ${mensagem.data[i].type}">
+            
+                <horario> (${mensagem.data[i].time})</horario>
+                <usuario>${mensagem.data[i].from}</usuario>
+                <mensagem>${mensagem.data[i].text}</mensagem>
+            
+                </ul>`  
+
+        } else if (mensagem.data[i].type === "private_message"){
+
+            //mensagem privada, mudar o "to" para "reservadamente para"
+
+                linhaMensagem.innerHTML += 
+                `
+                <ul class="mensagem ${mensagem.data[i].type}">
+            
+                <horario> (${mensagem.data[i].time})</horario>
+                <usuario>${mensagem.data[i].from}</usuario>
+                <span> reservadamente para </span>
+                <usuario>${mensagem.data[i].to}:</usuario>
+                <mensagem>${mensagem.data[i].text}</mensagem>
+            
+                </ul>`  
+
+        } else {
+
+            //mensagem para alguem especifico, mas nao privada o "alvo"
+
+                linhaMensagem.innerHTML += 
+                `
+                <ul class="mensagem ${mensagem.data[i].type}">
+            
+                <horario> (${mensagem.data[i].time})</horario>
+                <usuario>${mensagem.data[i].from}</usuario>
+                <span> para </span>
+                <usuario>${mensagem.data[i].to}:</usuario>
+                <mensagem>${mensagem.data[i].text}</mensagem>
+            
+                </ul>`  
+        }
     }
 }
 function importandoMensagemAPI(){
